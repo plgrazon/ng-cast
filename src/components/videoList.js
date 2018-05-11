d45500849af3354dfd819c9690b8a584e105c2b3
plgrazon <plgrazon@gmail.com>
2018-05-11 16:55:04 -0700

    Implement videoList,videoPlayer,videoListEntry to render

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,20 @@
 angular.module('video-player')
 .component('videoList', {
-  // TODO
+  controller: function () {
+
+  },
+
+  bindings: {
+    videos: '<'
+  },
+
+  template: `
+    <ul class="video-list">
+      <video-list-entry
+        video="video"
+        ng-repeat="video in $ctrl.videos track by $index"
+        /><h5><em>videoListEntry</em> component goes here</h5></video-list-entry>
+    </ul>
+  `
+
 });
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,18 @@
 angular.module('video-player')
 
 .component('videoPlayer', {
-  // TODO
+  controller: function() {
+    this.video = window.exampleVideoData[0];
+  },
+  template: `
+    <div class="video-player">
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" ng-src="https://www.youtube.com/embed/1w8Z0UOXVaY" allowFullScreen></iframe>
+      </div>
+      <div class="video-player-details">
+        <h3>{{ $ctrl.video.snippet.title }}</h3>
+        <div>{{ $ctrl.video.snippet.description }}</div>
+      </div>
+    </div>
+  `
 });
