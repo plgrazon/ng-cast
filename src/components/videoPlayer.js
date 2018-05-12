fafda43c9476c200e4ca36a657d3f29d73d9ce48
plgrazon <plgrazon@gmail.com>
2018-05-11 17:10:24 -0700

    Make videoPlayer show first video of videoList.

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -3,11 +3,12 @@ angular.module('video-player')
 .component('videoPlayer', {
   controller: function() {
     this.video = window.exampleVideoData[0];
+    this.videoURL = 'https://www.youtube.com/embed/' + this.video.id.videoId;
   },
   template: `
     <div class="video-player">
       <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" ng-src="https://www.youtube.com/embed/1w8Z0UOXVaY" allowFullScreen></iframe>
+        <iframe class="embed-responsive-item" ng-src="{{$ctrl.videoURL}}" allowFullScreen></iframe>
       </div>
       <div class="video-player-details">
         <h3>{{ $ctrl.video.snippet.title }}</h3>
